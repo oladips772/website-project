5b10ac0d157b351bb03e64f25ad619382ce726bc
oladips <oladips200@gmail.com>
2023-06-14 10:44:09 +0100

    h

---
+++ src/data/posts.js
@@ -0,0 +1,33 @@
+/** @format */
+const posts = [
+  {
+    title: "Lorem ipsum",
+    image:
+      "https://elementor.com/marketing/wp-content/uploads/sites/9/elementor/thumbs/02_Scrollytelling_Infrared-Mind-Body_-phu2c0ak7dak5onx85h3e93txmia1sgf76f7rt3qzs.jpg",
+    context:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, placeat. Perspiciatis, ipsum molestias nihil eum hic, animi obcaecati distinctio dolores aperiam quam, fugit repellat saepe omnis facilis suscipit pariatur praesentium?",
+  },
+  {
+    title: "Lorem ipsum",
+    image:
+      "https://elementor.com/marketing/wp-content/uploads/sites/9/elementor/thumbs/03_Horizontal-Scrolling_Sirup-phu2ci5ht7z0a9xzbv707mll7y2941fblmtfw2d9pk.png",
+    context:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, placeat. Perspiciatis, ipsum molestias nihil eum hic, animi obcaecati distinctio dolores aperiam quam, fugit repellat saepe omnis facilis suscipit pariatur praesentium?",
+  },
+  {
+    title: "Lorem ipsum",
+    image:
+      "https://elementor.com/cdn-cgi/image/f=auto,w=1024,h=597/marketing/wp-content/uploads/sites/9/2021/12/04_Brutalist-Typography_Zona-de-Propulsao.jpeg",
+    context:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, placeat. Perspiciatis, ipsum molestias nihil eum hic, animi obcaecati distinctio dolores aperiam quam, fugit repellat saepe omnis facilis suscipit pariatur praesentium?",
+  },
+  {
+    title: "Lorem ipsum",
+    image:
+      "https://elementor.com/cdn-cgi/image/f=auto,w=960/marketing/wp-content/uploads/sites/9/2020/12/05_Typography-Animation_Dilinger.png",
+    context:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, placeat. Perspiciatis, ipsum molestias nihil eum hic, animi obcaecati distinctio dolores aperiam quam, fugit repellat saepe omnis facilis suscipit pariatur praesentium?",
+  },
+];
+
+export default posts;
